c93c0ef34cdd4ae3d47270bbd52ede1bb494a765
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-02 17:25:10 +0700

    rename packages

--- src/main/java/finalProject/CoffeeShop/Controller/RegisterController.java
+++ src/main/java/finalProject/CoffeeShop/Controller/RegisterController.java
@@ -1,4 +1,4 @@
-package finalProject.Shopman.Controller;
+package finalProject.CoffeeShop.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/finalProject/CoffeeShop/FinalProjectSoftwareEngineeringApplication.java
+++ src/main/java/finalProject/CoffeeShop/FinalProjectSoftwareEngineeringApplication.java
@@ -1,4 +1,4 @@
-package finalProject.Shopman;
+package finalProject.CoffeeShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
