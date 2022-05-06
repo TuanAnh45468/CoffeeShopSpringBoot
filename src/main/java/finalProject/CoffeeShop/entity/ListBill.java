1f0ef931550606458415223fcb74602a9aa35a75
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-06 12:11:44 +0700

    create properties in ListBill Class

--- src/main/java/finalProject/CoffeeShop/entity/ListBill.java
+++ src/main/java/finalProject/CoffeeShop/entity/ListBill.java
@@ -1,5 +1,51 @@
 package finalProject.CoffeeShop.entity;
 
-public class ListBill {
+import java.util.Date;
 
+public class ListBill {
+	private int id;
+	private int userId;
+	private Date date;
+	
+	public ListBill() {
+			
+	}
+
+	public ListBill(int id, int userId, Date date) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "ListBill [id=" + id + ", userId=" + userId + ", date=" + date + "]";
+	}
+	
+	
 }
