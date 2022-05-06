ca1503febb4e556630177572804db8cb59ba6708
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-06 13:31:58 +0700

    create properties in Review class

--- src/main/java/finalProject/CoffeeShop/entity/Review.java
+++ src/main/java/finalProject/CoffeeShop/entity/Review.java
@@ -1,5 +1,70 @@
 package finalProject.CoffeeShop.entity;
 
 public class Review {
+	private int id;
+	private int userId;
+	private int productId;
+	private String comment;
+	private int rate;
+	
+	public Review() {
+		
+	}
 
+	public Review(int id, int userId, int productId, String comment, int rate) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.productId = productId;
+		this.comment = comment;
+		this.rate = rate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", userId=" + userId + ", productId=" + productId + ", comment=" + comment
+				+ ", rate=" + rate + "]";
+	}
+	
+	
 }
