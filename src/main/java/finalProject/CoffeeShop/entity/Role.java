c7a11d5956ca2d728e2d4eacabda492c1a6a4576
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-06 13:33:34 +0700

    create properties in Role class

--- src/main/java/finalProject/CoffeeShop/entity/Role.java
+++ src/main/java/finalProject/CoffeeShop/entity/Role.java
@@ -1,5 +1,40 @@
 package finalProject.CoffeeShop.entity;
 
 public class Role {
+	
+	private int id;
+	private String info;
+	
+	public Role() {
+		
+	}
 
+	public Role(int id, String info) {
+		super();
+		this.id = id;
+		this.info = info;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", info=" + info + "]";
+	}
+	
+	
 }
