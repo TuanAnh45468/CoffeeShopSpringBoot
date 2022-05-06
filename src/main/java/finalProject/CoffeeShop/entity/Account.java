990bfd6ae6e86d03c5d220b337c3d590d5187676
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-06 11:49:26 +0700

    add proterties to Account class

--- src/main/java/finalProject/CoffeeShop/entity/Account.java
+++ src/main/java/finalProject/CoffeeShop/entity/Account.java
@@ -2,4 +2,60 @@ package finalProject.CoffeeShop.entity;
 
 public class Account {
 
+	private int id;
+	private  int id_user;
+	private String name;
+	private String password;
+	
+	public Account() {
+		
+	}
+
+	public Account(int id, int id_user, String name, String password) {
+		super();
+		this.id = id;
+		this.id_user = id_user;
+		this.name = name;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", id_user=" + id_user + ", name=" + name + ", password=" + password + "]";
+	}
+	
+	
+	
 }
