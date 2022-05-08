e03b64f8aea3f02b3243442165329a3958d90778
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 10:17:13 +0700

    Create Role Repository

---
+++ src/main/java/finalProject/CoffeeShop/dao/RoleRepository.java
@@ -0,0 +1,9 @@
+package finalProject.CoffeeShop.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import finalProject.CoffeeShop.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
