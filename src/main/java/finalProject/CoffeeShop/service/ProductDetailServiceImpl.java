bc8049c234002730d6c2b8967b7ec4765b979ef0
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 15:30:52 +0700

    Implement methods in productDetail

--- src/main/java/finalProject/CoffeeShop/service/ProductDetailServiceImpl.java
+++ src/main/java/finalProject/CoffeeShop/service/ProductDetailServiceImpl.java
@@ -1,33 +1,56 @@
 package finalProject.CoffeeShop.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import finalProject.CoffeeShop.dao.ProductDetailRepository;
+import finalProject.CoffeeShop.entity.Account;
 import finalProject.CoffeeShop.entity.ProductDetail;
 
+@Service
 public class ProductDetailServiceImpl implements ProductDetailService {
-
+	private ProductDetailRepository productDetailRepository;
+	
+	@Autowired
+	public ProductDetailServiceImpl(ProductDetailRepository theProductDetailRepository) {
+		productDetailRepository = theProductDetailRepository;
+	}
+	
 	@Override
 	public List<ProductDetail> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return productDetailRepository.findAll();
 	}
 
 	@Override
 	public ProductDetail findById(int theId) {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<ProductDetail> result = productDetailRepository.findById(theId);
+		
+		ProductDetail productDetail = null;
+		
+		if(result.isPresent()) {
+			productDetail = result.get();
+		} else {
+			throw new RuntimeException("Did not find the account " + theId);
+		}
+		
+		return productDetail;
 	}
 
 	@Override
 	public void save(ProductDetail theProductDetail) {
 		// TODO Auto-generated method stub
-
+		productDetailRepository.save(theProductDetail);
 	}
 
 	@Override
 	public void deleteById(int theId) {
 		// TODO Auto-generated method stub
-
+		productDetailRepository.deleteById(theId);
 	}
 
 }
