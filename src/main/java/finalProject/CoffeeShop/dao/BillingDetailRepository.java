bd6fac2a965b7f99a66878a7957d28cfd162eceb
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 10:10:56 +0700

    add BillingDetail Repository

---
+++ src/main/java/finalProject/CoffeeShop/dao/BillingDetailRepository.java
@@ -0,0 +1,9 @@
+package finalProject.CoffeeShop.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import finalProject.CoffeeShop.entity.BillingDetail;
+
+public interface BillingDetailRepository extends JpaRepository<BillingDetail, Integer> {
+
+}
