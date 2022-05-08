62a089594ed44440eed6cd434ea4d6fdb0b99051
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 15:11:08 +0700

    implement methods

--- src/main/java/finalProject/CoffeeShop/service/AccountServiceImpl.java
+++ src/main/java/finalProject/CoffeeShop/service/AccountServiceImpl.java
@@ -1,33 +1,52 @@
 package finalProject.CoffeeShop.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import finalProject.CoffeeShop.dao.AccountRepository;
 import finalProject.CoffeeShop.entity.Account;
 
+@Service
 public class AccountServiceImpl implements AccountService {
+	
+	private AccountRepository accRepository;
 
+	@Autowired
+	public AccountServiceImpl(AccountRepository theAccountRepository) {
+		accRepository = theAccountRepository;
+	}
+	
 	@Override
 	public List<Account> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return accRepository.findAll();
 	}
 
 	@Override
 	public Account findById(int theId) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Account> result = accRepository.findById(theId);
+		
+		Account account = null;
+		
+		if(result.isPresent()) {
+			account = result.get();
+		} else {
+			throw new RuntimeException("Did not find the account " + theId);
+		}
+		
+		return account;
 	}
 
 	@Override
 	public void save(Account theAccount) {
-		// TODO Auto-generated method stub
-
+		accRepository.save(theAccount);
 	}
 
 	@Override
 	public void deleteById(int theId) {
-		// TODO Auto-generated method stub
-
+		accRepository.deleteById(theId);
 	}
 
 }
