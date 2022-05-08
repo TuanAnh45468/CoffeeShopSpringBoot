eb995517c074a60e9b2c1a989a41bd4870a2afbf
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 10:23:33 +0700

    create AdditionalInfo Servie

---
+++ src/main/java/finalProject/CoffeeShop/service/AdditionalInfoService.java
@@ -0,0 +1,15 @@
+package finalProject.CoffeeShop.service;
+
+import java.util.List;
+
+import finalProject.CoffeeShop.entity.AdditionalInfo;
+
+public interface AdditionalInfoService {
+	public List<AdditionalInfo> findAll();
+	
+	public AdditionalInfo findById(int theId);
+	
+	public void save(AdditionalInfo theEmployee);
+	
+	public void deleteById(int theId);
+}
---
+++ src/main/java/finalProject/CoffeeShop/service/AdditionalInfoServiceImpl.java
@@ -0,0 +1,33 @@
+package finalProject.CoffeeShop.service;
+
+import java.util.List;
+
+import finalProject.CoffeeShop.entity.AdditionalInfo;
+
+public class AdditionalInfoServiceImpl implements AdditionalInfoService {
+
+	@Override
+	public List<AdditionalInfo> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AdditionalInfo findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(AdditionalInfo theEmployee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
