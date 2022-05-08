419cd275221ebea567f99d755d8c0458dd75d3e0
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 10:10:04 +0700

    add AdditionalInfo Repository

---
+++ src/main/java/finalProject/CoffeeShop/dao/AdditionalInfoRepository.java
@@ -0,0 +1,9 @@
+package finalProject.CoffeeShop.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import finalProject.CoffeeShop.entity.AdditionalInfo;
+
+public interface AdditionalInfoRepository extends JpaRepository<AdditionalInfo, Integer> {
+
+}
