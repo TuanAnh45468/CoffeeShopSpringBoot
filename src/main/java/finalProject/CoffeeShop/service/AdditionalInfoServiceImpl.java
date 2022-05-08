a27e89c7bbee67bb9671c6a44decb8b3c4a65911
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-08 15:16:50 +0700

    Implement methods

--- src/main/java/finalProject/CoffeeShop/service/AdditionalInfoServiceImpl.java
+++ src/main/java/finalProject/CoffeeShop/service/AdditionalInfoServiceImpl.java
@@ -1,33 +1,51 @@
 package finalProject.CoffeeShop.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import finalProject.CoffeeShop.dao.AdditionalInfoRepository;
 import finalProject.CoffeeShop.entity.AdditionalInfo;
 
+@Service
 public class AdditionalInfoServiceImpl implements AdditionalInfoService {
 
+	private AdditionalInfoRepository infoRepository;
+	
+	@Autowired
+	public AdditionalInfoServiceImpl(AdditionalInfoRepository theInfoRepository) {
+		infoRepository = theInfoRepository;
+	}
+	
 	@Override
 	public List<AdditionalInfo> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return infoRepository.findAll();
 	}
 
 	@Override
 	public AdditionalInfo findById(int theId) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<AdditionalInfo> result = infoRepository.findById(theId);
+		
+		AdditionalInfo info = null;
+		
+		if(result.isPresent()) {
+			info = result.get();
+		} else {
+			throw new RuntimeException("Did not find the id " + theId);
+		}
+		return info;
 	}
 
 	@Override
 	public void save(AdditionalInfo theAdditionalInfo) {
-		// TODO Auto-generated method stub
-
+		infoRepository.save(theAdditionalInfo);
 	}
 
 	@Override
 	public void deleteById(int theId) {
-		// TODO Auto-generated method stub
-
+		infoRepository.deleteById(theId);
 	}
 
 }
