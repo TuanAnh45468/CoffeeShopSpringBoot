97342ba5ecf4599c6a7823d60f250f70317fb235
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-11 14:18:58 +0700

    fix gitignore file

---
+++ src/test/java/finalProject/Shopman/FinalProjectSoftwareEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package finalProject.Shopman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSoftwareEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
