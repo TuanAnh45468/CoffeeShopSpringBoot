08aff29b6c61dbf9998b426ce7111f4efbdbef98
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-16 16:23:12 +0700

    fix log in successfully

--- src/main/java/finalProject/CoffeeShop/Config/SecurityConfiguration.java
+++ src/main/java/finalProject/CoffeeShop/Config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated()
 				.and()
-				.formLogin().loginPage("/login").permitAll().failureUrl("/login?error=true").defaultSuccessUrl("/about-us").usernameParameter("email").passwordParameter("password")
+				.formLogin().loginPage("/login").permitAll().failureUrl("/login?error=true").defaultSuccessUrl("/").usernameParameter("email").passwordParameter("password")
 				.and()
 				.logout()
 				.invalidateHttpSession(true).clearAuthentication(true)
