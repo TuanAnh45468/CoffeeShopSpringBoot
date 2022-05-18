68b2f7aa1536bf082e276331da903aca29d34070
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-18 22:48:46 +0700

    Fix security not login

--- src/main/java/finalProject/CoffeeShop/Config/SecurityConfiguration.java
+++ src/main/java/finalProject/CoffeeShop/Config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/login", "/**").permitAll()
+		http.authorizeRequests().antMatchers("/login").permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and()
