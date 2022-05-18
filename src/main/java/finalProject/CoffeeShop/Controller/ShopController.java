15d7fd75c25d4c7b160869af02ab36c1124ca098
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-18 16:35:27 +0700

    display product in main screen

--- src/main/java/finalProject/CoffeeShop/Controller/ShopController.java
+++ src/main/java/finalProject/CoffeeShop/Controller/ShopController.java
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import finalProject.CoffeeShop.entity.ProductDetail;
 import finalProject.CoffeeShop.entity.User;
+import finalProject.CoffeeShop.service.ProductDetailService;
 import finalProject.CoffeeShop.service.RoleService;
 import finalProject.CoffeeShop.service.UserService;
 
 @Controller
 public class ShopController {
+	
+	@Autowired
+	private ProductDetailService productDetailService;
 	
 	@GetMapping("/login")
 	public String login() {
@@ -27,7 +32,8 @@ public class ShopController {
 	//mapping for homepage
 	@GetMapping("/")
 	public String index(Model model) {
-		
+		//ProductDetail productDetail = new ProductDetail();
+		model.addAttribute("products", productDetailService.findAll());
 		return "index";
 	}
 }
